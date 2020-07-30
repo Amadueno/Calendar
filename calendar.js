392fbc99d5e0991d7c8f584dc5165e56ba52dc8d
Adam Madueno <amadueno17@yahoo.com>
2020-07-29 23:39:55 -0700

    added to html

--- calendar.js
+++ calendar.js
@@ -1,2 +1,32 @@
-$('#currentDay').html(moment().format('dddd, MMMM Do, YYYY'));
 
+$('#currentDay').html(moment().format('dddd, MMMM Do, YYYY'));
+ 
+let time24 = moment ().format('H');
+let hour12 = moment ().format ('h', 'm')
+
+console.log(hour12)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.saveBtn').click(function (event) {
+
+    event.preventDefault()
+    
+   
+
+    
+
+
+
+})
